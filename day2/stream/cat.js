70dbc436291b44e73aa60880625dc98ffb1a6494
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-03 11:34:05 -0500

    concat server example

---
+++ day2/stream/cat.js
@@ -0,0 +1,25 @@
+var concat = require('concat-stream')
+var through = require('through2')
+var http = require('http')
+var qs = require('querystring')
+
+var server = http.createServer(function (req, res) {
+  req
+    .pipe(counter())
+    .pipe(concat({ encoding: 'string' }, onbody))
+
+  function counter () {
+    var size = 0
+    return through(function (buf, enc, next) {
+      size += buf.length
+      if (size > 20) next(null, null)
+      else next(null, buf)
+    })
+  }
+  function onbody (body) {
+    var params = qs.parse(body)
+    console.log(params)
+    res.end('ok\n')
+  }
+})
+server.listen(5000)
