7fe35f21e335dede4d41172c91b427ad84173457
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-05 14:58:24 -0500

    feedback etc

--- day4/webgl/main.js
+++ day4/webgl/main.js
@@ -1,11 +1,13 @@
 var regl = require('regl')()
 var camera = require('regl-camera')(regl, { distance: 4 })
-var icosphere = require('icosphere')
+//var icosphere = require('icosphere')
+var bunny = require('bunny')
 var anormals = require('angle-normals')
 var glsl = require('glslify')
+var feedback = require('regl-feedback')
 
 function createBlob (regl) {
-  var mesh = icosphere(3)
+  var mesh = bunny //icosphere(3)
   return regl({
     frag: glsl`
       precision highp float;
@@ -15,7 +17,7 @@ function createBlob (regl) {
       uniform float time, stage;
       void main () {
         gl_FragColor = vec4((vnorm+1.0)*0.5 * vec3(0,1,0)
-          + hsl2rgb(snoise(vec4(vpos,time*0.1+stage)),1.0,0.5)
+          + hsl2rgb(stage*0.3+0.1*snoise(vec4(vpos,time*0.1+stage)),1.0,0.5)
           ,1);
       }
     `,
@@ -31,7 +33,7 @@ function createBlob (regl) {
         vpos = position;
         vnorm = normal;
         gl_Position = projection * view
-          * vec4(position + location
+          * vec4(position*0.2 + location
             + normal*snoise(vec4(position,time+stage))*0.1,1);
       }
     `,
@@ -49,16 +51,24 @@ function createBlob (regl) {
 }
 
 var draw = {
-  blob: createBlob(regl)
+  blob: createBlob(regl),
+  fb: feedback(regl, `
+    vec3 sample (vec2 uv, sampler2D tex) {
+      return 0.97*texture2D(tex, (0.99*(2.0*uv-1.0)+1.0)*0.5).rgb;
+    }
+  `)
 }
+var fbtex = regl.texture()
 
 regl.frame(function () {
-  regl.clear({ color: [0,1,1,1], depth: true })
+  regl.clear({ color: [0,0,0,1], depth: true })
+  draw.fb({ texture: fbtex })
   camera(function () {
     draw.blob([
       { location: [0,0,0], stage: 0 },
       { location: [0,0,-2], stage: 1 },
       { location: [0,0,2], stage: 2 }
     ])
+    fbtex({ copy: true, mag: 'linear', min: 'linear' })
   })
 })
